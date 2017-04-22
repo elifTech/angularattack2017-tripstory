864b9d80c508025edef124e414d60681044744ab
vitaliisavchuk <vitaliisavchuk@cloud.upwork.com>
2017-04-22 08:40:54 +0300

    add configs for env

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@ import {CustomConfig} from 'ng2-ui-auth';
 
 export const IS_DEV = ENV === 'development';
 export const API_URL = IS_DEV ? 'http://localhost:4040' : 'https://eliftech-tripstory.herokuapp.com';
-export const FACEBOOK_APP_ID = IS_DEV ? '418197618334612' : '';
+export const FACEBOOK_APP_ID = IS_DEV ? '418197618334612' : '1876751672543517';
 
 export class MyAuthConfig extends CustomConfig {
   defaultHeaders = {'Content-Type': 'application/json'};
